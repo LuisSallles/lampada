d5013316aa39de7019a68e50fb077ed80d5fe804
Luis Salles <107654852+LuisSallles@users.noreply.github.com>
2023-09-15 20:33:01 -0300

    .

--- lampada.js
+++ lampada.js
@@ -1,10 +1,12 @@
-const result = document.getElementById('lamp-result')
+    const result = document.getElementById('lamp-result')
 
 function lampCondition(lampOn, lampOff, result) {  // Function condition, if on = return Lamp On, if off = return Lamp off
     if(lampOn) {
-        return alert('Turn On')
-    } else {
-        return alert('Turn Off')
+        result.document.createElement('imagens/lamp-on.jpg')
+        return 
+    } else if (lampOff){
+        result.document.createElement('imagens/lamp-off.jpg')
+        return 
     }
 }
 
