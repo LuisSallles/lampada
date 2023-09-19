5cd11039242f513f91a8bf32c786329e671fb631
Luis Salles <107654852+LuisSallles@users.noreply.github.com>
2023-09-18 21:04:09 -0300

    .

--- lampada.js
+++ lampada.js
@@ -5,12 +5,10 @@ function insertImg(on) {
     img.setAttribute('id', 'lamp-off')
     img.setAttribute('src', 'imagens/lamp-off.jpg')
     if(on) {
-        on === ``
         return result.appendChild(img)
     }
 }
 
 function turnOff() { // Here the lamp will turn off by click
-    const turnOn = document.getElementById('lamp-on')
-    insertImg(turnOn)
+    
 }
